e0807c3138205258eae6353adf94c9fced65768e
jp039 <167986374+jp039@users.noreply.github.com>
2024-04-24 17:20:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript Interactivity
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Bootstrap carousel
+    var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExampleIndicators'), {
+      interval: 5000,
+      keyboard: true,
+      ride: true
+    });
+  });
+  
